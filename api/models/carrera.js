b938a88b14d947a5d04c6bf36cb3325a9b06f616
matycris <crisnejomatias@gmail.com>
2023-05-20 16:42:55 -0300

    aaaa

--- api/models/carrera.js
+++ api/models/carrera.js
@@ -4,5 +4,15 @@ module.exports = (sequelize, DataTypes) => {
     nombre: DataTypes.STRING
   }, {});
   
+  //codigo de asociacion  (tiene muchos:)
+  carrera.associate = function(models) {
+  	carrera.hasMany(models.materia,  // Modelo al que pertenece
+    {
+      as: 'materia',                 // nombre de mi relacion
+      foreignKey: 'id_carrera'       // campo con el que voy a igualar 
+    })
+  };
+  ///////////////////////
+
   return carrera;
 };
